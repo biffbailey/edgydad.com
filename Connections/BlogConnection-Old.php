c1b290d18a826b42fd9ee169d36ebded7e48c061
EdgyDad <edgyda@edgydad.com>
2017-07-13 11:26:04 -0500

    save old connection file, change variables to gcloud sql

--- Connections/BlogConnection-Old.php
+++ Connections/BlogConnection-Old.php
@@ -2,9 +2,9 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_BlogConnection = "/cloudsql/edgydad-168800:edgydad-sql-0";
+$hostname_BlogConnection = "dbdata1.db.3273326.hostedresource.com";
 $database_BlogConnection = "dbdata1";
-$username_BlogConnection = "root";
+$username_BlogConnection = "dbdata1";
 $password_BlogConnection = "Ironman1726!";
 $BlogConnection = mysql_pconnect($hostname_BlogConnection, $username_BlogConnection, $password_BlogConnection) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
